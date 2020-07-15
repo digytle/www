84a72749c93fdbf78b37cd00fb2da820429131eb
Milen Markov <m1@auxcode.com>
2020-07-15 12:13:59 +0300

    LatestWork component was made of the design

--- src/components/LatestWork.vue
+++ src/components/LatestWork.vue
@@ -1,15 +1,20 @@
 <template>
   <div class="container">
     <div class="row">
-      <div class="col-12 row">
+      <div class="col-12 row header-text">
         <h1>{{headerText}}</h1>
+      </div>
+      <div class="col-12 row header-text">
         <p>{{headerDescription}}</p>
       </div>
-      <div class="col-12 row">
+      <div class="col-12 row section">
         <img src="../../public/assets/blits.png" />
         <img src="../../public/assets/Asset 8-100.png" />
         <div class="image-box">
           <p>{{title}}</p>
+          <button>
+            <b-icon class="arrow-icon" icon="arrow-right"></b-icon>
+          </button>
         </div>
       </div>
     </div>
@@ -36,10 +41,46 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.image-box {
-  width: 350px;
-  height: 350px;
-  background-color: #650fba;
-  color: white;
+.header-text {
+  justify-content: center;
+  h1 {
+    font-weight: 700;
+    font-size: 40px;
+    color: #333333;
+    margin-bottom: 40px;
+  }
+  p {
+    width: 860px;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 20px;
+    color: #7a7a7a;
+    margin-bottom: 80px;
+  }
+}
+.section {
+  justify-content: space-between;
+  margin-bottom: 170px;
+  .image-box {
+    width: 350px;
+    height: 350px;
+    background-color: #650fba;
+    color: white;
+    p {
+      margin-top: 100px;
+      margin-bottom: 30px;
+      font-weight: 700;
+      font-size: 40px;
+    }
+    button {
+      border-radius: 100%;
+      padding: 20px 23px;
+      border: none;
+      background-color: white;
+      .arrow-icon {
+        color: #c971f3;
+      }
+    }
+  }
 }
 </style>
