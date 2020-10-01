e867b3c22d578011808c8b223d774f348b3d9142
Misho <mn@auxcode.com>
2020-10-01 21:04:52 +0300

    PrivacyPolicy page

---
+++ src/components/PrivacyPolicy.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="privacy-policy">
+    <Navigation></Navigation>
+    <div class="row privacy">
+      <h1>{{ data.title }}</h1>
+      <div class="content" v-for="section in data.content" :key="section.text">
+        <h2>{{ section.sectionTitle }}</h2>
+        <p>{{ section.text }}</p>
+      </div>
+    </div>
+    <Footer></Footer>
+  </div>
+</template>
+<script>
+import Navigation from "./Navigation";
+import navigation from "js-yaml-loader!../../content/navigation.yaml";
+import Footer from "./Footer";
+export default {
+  name: "PrivacyPolicy",
+  components: { Navigation, Footer },
+  data() {
+    return {
+      data: {},
+    };
+  },
+  mounted() {
+    this.data = require(`js-yaml-loader!../../content/privacy-policy.yaml`);
+    this.logoText = navigation["logo-text"];
+    this.home = navigation.home;
+    this.about = navigation.about;
+    this.work = navigation.work;
+    this.contacts = navigation.contacts;
+  },
+};
+</script>
+
+<style scoped lang="scss">
+@import "../assets/main.scss";
+.privacy-policy {
+  .privacy {
+    margin-left: 170px;
+    text-align: left;
+    h1 {
+      margin-top: 100px;
+      color: #2b2d42;
+      margin-bottom: 100px;
+    }
+    .content {
+      h2 {
+        font-size: 30px;
+        color: #2b2d42;
+        margin-bottom: 30px;
+      }
+      p {
+        width: 1025px;
+        margin-bottom: 60px;
+        color: #8d99ae;
+      }
+    }
+  }
+}
+</style>
