03c460d725272667c41c6dd6ca1115bbb8ceac63
T. E. Shaw <t.e.shaw@dahoum.wales>
2021-04-10 18:57:37 +0200

    Netlify Form in header

--- src/components/Template.vue
+++ src/components/Template.vue
@@ -84,27 +84,11 @@
       </div>
     </div>
     
-    <form name="contact" method="POST" netlify action="/">
-      <p>
-        <label>Your Name: <input type="text" name="name" /></label>   
-      </p>
-      <p>
-        <label>Your Email: <input type="email" name="email" /></label>
-      </p>
-      <p>
-        <label>Your Role: <select name="role[]" multiple>
-          <option value="leader">Leader</option>
-          <option value="follower">Follower</option>
-        </select></label>
-      </p>
-      <p>
-        <label>Message: <textarea name="message"></textarea></label>
-      </p>
-      <p>
-        <button type="submit">Send</button>
-      </p>
-    </form>
-
+    <!--
+      The form must be unique.
+      If you inclue it in mutiple pages (which you do)——you must add something to the name.
+      For example the yaml page name.
+      -->
     <form class="contact" name="contact" method="POST" data-netlify="true">
       <h2>{{ contact.text.heading }}</h2>
       <p>{{ contact.text.subheading }}</p>
