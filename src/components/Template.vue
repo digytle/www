e107364f0ebf045b18bdfa4724ebd3409b3fe0be
T. E. Shaw <t.e.shaw@dahoum.wales>
2021-04-10 18:39:30 +0200

    Netlify Form action

--- src/components/Template.vue
+++ src/components/Template.vue
@@ -84,7 +84,7 @@
       </div>
     </div>
     
-    <form name="contact" method="POST" data-netlify="true">
+    <form name="contact" method="POST" data-netlify="true" action="/">
       <p>
         <label>Your Name: <input type="text" name="name" /></label>   
       </p>
