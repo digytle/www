3e3941ed3c9586f71d22beb63899b4b1e503e699
ivelinvladimirov <71154311+ivelinvladimirov@users.noreply.github.com>
2021-04-12 19:00:39 +0300

    email form bug

--- src/components/contactForm.vue
+++ src/components/contactForm.vue
@@ -2,12 +2,8 @@
     <form name="Contact-us" method="post" data-netlify="true">
         <input type="hidden" name="form-name" value="Contact-us" />
         <p>
-            <label>Your Name: <input type="text" name="name" /></label>   
-        </p>
-        <p>
+            <label>Your Name: <input type="text" name="name" /></label>  
             <label>Your Email: <input type="text" name="email" /></label>
-        </p>
-        <p>
             <label>Message: <textarea name="text"></textarea></label>
         </p>
         <button type="submit">go</button>
