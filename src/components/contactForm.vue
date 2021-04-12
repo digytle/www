afca5656c16e8ced948b94db2f6b20a1625fd046
ivelinvladimirov <71154311+ivelinvladimirov@users.noreply.github.com>
2021-04-12 18:20:38 +0300

    empty form bug

--- src/components/contactForm.vue
+++ src/components/contactForm.vue
@@ -2,9 +2,9 @@
     <div class="Contact">
         <form name="Contact-us" method="post" data-netlify="true">
             <input type="hidden" name="form-name" value="Contact-us" />
-            <input name="name" placeholder="First name">
-            <input name="Email" placeholder="Email">
-            <input name="Message" placeholder="description">
+            <input type="text" name="name" placeholder="First name">
+            <input type="text" name="Email" placeholder="Email">
+            <input type="text" name="Message" placeholder="description">
             <button type="submit">go</button>
         </form>
     </div>
