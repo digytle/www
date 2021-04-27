ad7f2f14501e03ca7273bd9f428428cdf5adeb84
ivelinvladimirov <71154311+ivelinvladimirov@users.noreply.github.com>
2021-04-27 20:03:12 +0300

    Revert "sitemap test"
    This reverts commit e65f1a5e8f277b6e142e8a96614bd7a31e1c3bfe.

--- babel.config.js
+++ babel.config.js
@@ -3,26 +3,3 @@ module.exports = {
     '@vue/cli-plugin-babel/preset'
   ]
 }
-
-const SitemapPlugin = require('sitemap-webpack-plugin').default;
-const paths = [
-    {
-        path: '/',
-        lastmod: new Date().toISOString().slice(0,10),
-        priority: '0.8',
-        changefreq: 'hourly'
-    },
-];
-
-module.exports = {
-    configureWebpack: {
-        plugins: [
-            new SitemapPlugin('https://acceptance.digytle.com/', paths, {
-                filename: 'sitemap.xml',
-                lastmod: true,
-                changefreq: 'hourly',
-                priority: '0.8'
-            })
-        ]
-    }
-}
