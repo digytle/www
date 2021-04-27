6ea81f38a08283afab924a774a859f8a5b478a19
ivelinvladimirov <71154311+ivelinvladimirov@users.noreply.github.com>
2021-04-27 19:43:21 +0300

    sitemap test

--- babel.config.js
+++ babel.config.js
@@ -4,25 +4,3 @@ module.exports = {
   ]
 }
 
-const SitemapPlugin = require('sitemap-webpack-plugin').default;
-const paths = [
-    {
-        path: '/',
-        lastmod: new Date().toISOString().slice(0,10),
-        priority: '0.8',
-        changefreq: 'hourly'
-    },
-];
-
-module.exports = {
-    configureWebpack: {
-        plugins: [
-            new SitemapPlugin('https://acceptance.digytle.com/', paths, {
-                filename: 'sitemap.xml',
-                lastmod: true,
-                changefreq: 'hourly',
-                priority: '0.8'
-            })
-        ]
-    }
-}
